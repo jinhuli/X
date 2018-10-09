d2726400dada55576a9c94c78b3aaa171480f206
xuzhipeng <xuzhipenganhui@hotmail.com>
2018-10-09 17:17:54 +0800

    11

---
+++ tools/easyBC.py
@@ -0,0 +1,17 @@
+from WindPy import *
+from datetime import *
+import pandas as pd
+w.start()
+class easybc(object):
+    def __init__(self,startdate,enddate,frequnce,list):
+        self.startdate = startdate
+        self.endtdate = enddate
+        self.frequnce = frequnce
+        self.list = list
+
+
+    def __bardata(self):
+        pass
+
+
+
