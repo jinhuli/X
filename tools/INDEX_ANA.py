73cfb4c9b8220e6be6844866b02a2c860ffed562
xuzhipeng <xuzhipenganhui@hotmail.com>
2018-10-11 18:14:24 +0800

    11

--- tools/INDEX_ANA.py
+++ tools/INDEX_ANA.py
@@ -3,6 +3,9 @@ from datetime import *
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from docx import Document
+import matplotlib
+matplotlib.matplotlib_fname()
 w.start()
 
 
@@ -42,12 +45,12 @@ class index(object):
         self.data = None
         self.indextimeseries = None
         codelist = w.wset("indexconstituent", "date=" + self.tradate + ";windcode=" + self.indexCode)
-        self.list = pd.DataFrame(codelist.Data, columns=codelist.Codes, index=codelist.Fields).T["wind_code"].tolist()
+        self.list = pd.DataFrame(codelist.Data, columns=codelist.Codes, index=codelist.Fields,dtype=float).T["wind_code"].tolist()
 
 ##########获取指数成分#####
     def get_code_list(self):
         codelist = w.wset("indexconstituent","date="+self.tradate+";windcode="+self.indexCode)
-        self.codelist = pd.DataFrame(codelist.Data, columns=codelist.Codes, index=codelist.Fields).T
+        self.codelist = pd.DataFrame(codelist.Data, columns=codelist.Codes, index=codelist.Fields,dtype=float).T
         return self.codelist
     ###获取指数截面因子#####
     def get_factor(self):
@@ -56,7 +59,7 @@ class index(object):
         year = datetime.strptime(self.tradate, '%Y-%m-%d').year
         ind="pct_chg_per,val_pe_deducted_ttm,pb_lyr,dividendyield2,pe_est,est_peg,BOLL,DMA,industry_sw,val_lnfloatmv,val_floatmv"
         factor =w.wss(self.list,ind,"startDate=%s;endDate=%s;tradeDate=%s;year=%s;rptYear=%s;BOLL_N=26;BOLL_Width=2;BOLL_IO=1;priceAdj=F;cycle=D;DMA_S=10;DMA_L=50;DMA_N=10;DMA_IO=1;industryType=1" %(startdate,tradedate,tradedate,year,year))
-        self.factor = pd.DataFrame(factor.Data, columns=factor.Codes, index=factor.Fields).T
+        self.factor = pd.DataFrame(factor.Data, columns=factor.Codes, index=factor.Fields,dtype=float).T
         return self.factor
 
     def get_data(self):
@@ -69,7 +72,7 @@ class index(object):
 
     def get_indextimeseries(self):
         data = w.wsd(self.indexCode,"close,pct_chg", "ED-5Y",self.tradate, "")
-        self.indextimeseries = pd.DataFrame(data.Data, columns=data.Times, index=data.Fields).T
+        self.indextimeseries = pd.DataFrame(data.Data, columns=data.Times, index=data.Fields,dtype=float).T
         self.indextimeseries["PCT_CHG_1"] = self.indextimeseries["PCT_CHG"].map(lambda x: x / 100 + 1)
         self.indextimeseries["nav"] = self.indextimeseries["PCT_CHG_1"].cumprod()
 
@@ -77,31 +80,61 @@ class index(object):
 
 
 
+##因子分组函数
+def cla(n, lim):
+    return '[%.f ,%.f)' % (lim * (n // lim), lim * (n // lim) + lim)
 
-
+  ##  b = data["VAL_FLOATMV"].apply(cla, args=(100,)).values
+  ##  data["%s_3" % factor.upper()] = b
 
 
 
 if __name__ == '__main__':
-    from docx import Document
+    ####准备数据
     zz500=index("2018-9-26","000905.SH","000905.SH")
     zz500.get_code_list()
     zz500.get_factor()
     zz500.get_data()
     zz500.get_indextimeseries()
     timeseries = zz500.indextimeseries
+    data = zz500.get_data()
+    ##净值走势图
     aa=plt.figure()
     timeseries["nav"].plot()
     plt.savefig("E:\\github\\X\\2.jpg")
-    plt.show()
+
+    document = Document()  #创建文档类
+    document.add_heading("中证500指数分析", 0)
+    p = document.add_paragraph('本报告由python程序自动生成，数据来自于WIND')
+    document.add_heading('净值走势')
+    from docx.shared import Inches
+    document.add_picture("E:\\github\\X\\2.jpg",width=Inches(4.0))
+
+    ###因子权重分析
+    document.add_heading('因子权重分析')
+    data[["i_weight"]] = data[["i_weight"]].astype(float)
+    ##分组
+    factor_i=['PCT_CHG_PER', 'VAL_PE_DEDUCTED_TTM','PB_LYR', 'DIVIDENDYIELD2', 'PE_EST', 'EST_PEG', 'BOLL', 'DMA',
+       'INDUSTRY_SW', 'VAL_LNFLOATMV', 'VAL_FLOATMV',"i_weight"]
+    factor_d =
+    data["VAL_FLOATMV_group"] = data["VAL_FLOATMV"].rank().apply(cla, args=(100,)).values
+    ##writeTable(data.head(),document)
 
 
-    document = Document()
-    data=zz500.get_data()
+    ##因子相关性
+    document.add_heading('因子相关性分析')
 
-    writeTable(data,document)
+    #####收益归因--分组收益统计####
+    document.add_heading('指数收益归因')
+
+    document.add_heading('行业归因',level = 2)
+
+    document.add_heading('风格归因',level = 2)
+
     document.save("E:\\github\\X\\demo.docx")
-    print("end")
+
+
+    cla(100,200)
 
 
 
