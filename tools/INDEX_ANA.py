bc1752c7619e56baae194b2eb0ccc8ccb7c97060
xuzhipeng <xuzhipenganhui@hotmail.com>
2018-10-12 18:05:24 +0800

    11

--- tools/INDEX_ANA.py
+++ tools/INDEX_ANA.py
@@ -3,6 +3,14 @@ from datetime import *
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+# 解决中文乱码问题
+#sans-serif就是无衬线字体，是一种通用字体族。
+#常见的无衬线字体有 Trebuchet MS, Tahoma, Verdana, Arial, Helvetica, 中文的幼圆、隶书等等。
+mpl.rcParams['font.sans-serif']=['SimHei'] #指定默认字体 SimHei为黑体
+mpl.rcParams['axes.unicode_minus']=False #用来正常显示负号
+plt.style.use('ggplot')
+
 from docx import Document
 import matplotlib
 matplotlib.matplotlib_fname()
@@ -82,16 +90,22 @@ class index(object):
 
 ##因子分组函数
 def cla(n, lim):
-    return '[%.f ,%.f)' % (lim * (n // lim), lim * (n // lim) + lim)
+    return '[%.f ,%.f)' % (lim * ((n-0.01 )// lim), lim * ((n-0.01 ) // lim) + lim)
 
   ##  b = data["VAL_FLOATMV"].apply(cla, args=(100,)).values
   ##  data["%s_3" % factor.upper()] = b
+
+def visual(data,title, x=None, y=None):
+    fig, ax = plt.subplots()
+    ax.plot(x, y)
+    plt.show()
+
 
 
 
 if __name__ == '__main__':
     ####准备数据
-    zz500=index("2018-9-26","000905.SH","000905.SH")
+    zz500=index("2018-9-26", "000905.SH", "000905.SH")
     zz500.get_code_list()
     zz500.get_factor()
     zz500.get_data()
@@ -108,16 +122,63 @@ if __name__ == '__main__':
     p = document.add_paragraph('本报告由python程序自动生成，数据来自于WIND')
     document.add_heading('净值走势')
     from docx.shared import Inches
-    document.add_picture("E:\\github\\X\\2.jpg",width=Inches(4.0))
+    document.add_picture("E:\\github\\X\\2.jpg", width=Inches(4.0))
 
     ###因子权重分析
+    factor_i = ['PCT_CHG_PER', 'VAL_PE_DEDUCTED_TTM', 'PB_LYR', 'DIVIDENDYIELD2', 'PE_EST', 'EST_PEG',
+                'BOLL', 'DMA', 'VAL_LNFLOATMV', 'VAL_FLOATMV']
+    data[factor_i] = data[factor_i].astype(float)
+    factor_d = []
     document.add_heading('因子权重分析')
-    data[["i_weight"]] = data[["i_weight"]].astype(float)
+      ###因子分组均分成5组
+    for i in factor_i:
+        data[i+"_group"] = data[i].rank(ascending=False).apply(cla, args=(100,)).values
+
+    pie_data1=data.groupby("INDUSTRY_SW")["i_weight"].sum().sort_values()
+    fig1, ax1 = plt.subplots()
+    ax1.pie(pie_data1,labels=pie_data1.index.tolist(),shadow=True, autopct='%1.1f%%', startangle=90)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    plt.title("因子权重")
+    plt.show()
+    plt.savefig("E:\\github\\X\\1.jpg")
+    document.add_picture("E:\\github\\X\\1.jpg", width=Inches(4.0))
+
+    fig, axs = plt.subplots(4, 2, figsize=(5, 5))
+    for i in range(len(factor_i)):
+        factor=factor_i[i]
+        pie_data = data.groupby(factor + "_group")["i_weight"].sum().sort_values()
+        a=i//2
+        b=i%2
+        axs[a, b].pie(pie_data, labels=pie_data.index.tolist(), shadow=True, autopct='%1.1f%%', startangle=90)
+        axs[a, b].set_title(factor)
+    plt.show()
+
+
+
+
+
+
+
+
+
+    ###因子交叉分析
+    dddata = data.groupby( ['VAL_PE_DEDUCTED_TTM_group','EST_PEG_group'])["i_weight"].sum()
+    x= dddata.index.levels[1].tolist()
+    y = dddata.index.levels[0].tolist()
+    X,Y =np.meshgrid(x,y)
+    def zz(x,y):
+        return dddata[x][y]
+
+    Z=dddata.map(zz)
+
+
+
+
+
+    document.add_picture("E:\\github\\X\\3.jpg", width=Inches(4.0))
+
     ##分组
-    factor_i=['PCT_CHG_PER', 'VAL_PE_DEDUCTED_TTM','PB_LYR', 'DIVIDENDYIELD2', 'PE_EST', 'EST_PEG', 'BOLL', 'DMA',
-       'INDUSTRY_SW', 'VAL_LNFLOATMV', 'VAL_FLOATMV',"i_weight"]
-    factor_d =
-    data["VAL_FLOATMV_group"] = data["VAL_FLOATMV"].rank().apply(cla, args=(100,)).values
+
     ##writeTable(data.head(),document)
 
 
