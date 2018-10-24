5157dac7f6302836790c5006c521034d86f8769a
xuzhipeng <xuzhipenganhui@hotmail.com>
2018-10-24 17:38:22 +0800

    11

--- tools/toMysql.py
+++ tools/toMysql.py
@@ -1,41 +1,48 @@
 #!/usr/bin/env python
-from sqlalchemy import Column, String, create_engine
+from sqlalchemy import create_engine,Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import sqlalchemy
 
-
-
-
-engine = create_engine('mysql+pymysql://root:8261426@localhost:3306/test?charset=utf8')
 Base = declarative_base()
 
 class MySQLAlchemy(object):
-    def __init__(self, db, dbName,User):
-        import pymysql
+    def __init__(self, db, dbName):
+
         url = "mysql+pymysql://root:8261426@localhost:3306/%s" % dbName
+        eng = create_engine(url)
+        Base = declarative_base()
+        Base.metadata.create_all(eng)
+        Session = sessionmaker(bind=eng)
+        self.session = Session()
+        self.db = db
+        self.eng = eng82
+        ##Base.metadata.create_all(self.eng)
+    def insert(self,user):
+        self.session.add(user)
+        self.session.commit()
 
-    eng = create_engine(url)
+    def query(self):
+        pass
+
+    def close(self):
+        self.eng.dispose()
 
 
+from sqlalchemy import Sequence
+Column(Integer, Sequence('user_id_seq'), primary_key=True)
 class User(Base):
-
     __tablename__ = 'users'
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    name = Column(String(50))
+    fullname = Column(String(50))
+    password = Column(String(12))
 
-    id = Column(String(20), primary_key=True)
-    name = Column(String(20))
     def __repr__(self):
-        return '%s(%r)' % (self.__class__.__name__, self.username)
+        return "<User(name='%s', fullname='%s', password='%s')>" % (
+                                self.name, self.fullname, self.password)
+
+ed_user = User(name='ed', fullname='Ed Jones', password='edspassword')
 
 
-DBSession = sessionmaker(bind=engine)
-
-# 创建session对象:
-session = DBSession()
-# 创建新User对象:
-new_user = User(id='5', name='Bob')
-# 添加到session:
-session.add(new_user)
-# 提交即保存到数据库:
-session.commit()
-# 关闭session:
-session.close()
+con =  MySQLAlchemy("we","test")
