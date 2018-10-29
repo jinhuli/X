e69e07b565625aaf76c759a2abcb830da4f7907a
xuzhipeng <xuzhipenganhui@hotmail.com>
2018-10-29 17:45:28 +0800

    11

--- tools/GetMbReport.py
+++ tools/GetMbReport.py
@@ -1,16 +1,143 @@
 #coding=GBK
 import queue
 import threading
-import urllib2
+import urllib.request
+import http.cookiejar
 import time
 import re
-from BeautifulSoup import *
-from urlparse import urljoin
-import pyodbc
-from PAM30 import PAMIE
-import Queue
-import string
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
+from bs4 import *
+import urllib.parse
+pages = queue.Queue()
+
+url = 'http://www.hibor.com.cn/result.asp?lm=0&area=DocTitle&timess=13&key=&dtype=&page=1'
+class ThreadUrl(threading.Thread):
+    def __init__(self, pages):
+        threading.Thread.__init__(self)
+        self.conn = pymysql.connect("localhost","root","8261426","stock"
+                                  ,use_unicode=True, charset="utf8")
+        self.cursor =self.conn.cursor()
+        self.pages = pages
+        self.headers=["User-Agent","Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"]
+        self.postdata = urllib.parse.urlencode({"username":"xuzhipeng8","password":"xuzhipeng8261426"}).encode("utf-8")
+        req = urllib.request.Request(url,self.postdata)
+        req.add_header(self.headers[0],self.headers[1])
+        data = urllib.request.urlopen(req).read()
+    def run(self):
+
+        while True:
+
+            try:
+
+                TotalAbstract = ''
+                page = pages.get()
+                try:
+                    ie1.navigate(page)
+                    chunk = ie1.getPageText()
+                except:
+                    try:
+                        ie1.navigate(page)
+                        chunk = ie1.getPageText()
+                    except:
+                        ie1.navigate(page)
+                        chunk = ie1.getPageText()
+                print('souping+' + page + '\n')
+
+                soup = BeautifulSoup(chunk)
+                soup1 = soup.find('div', {"class": "baogaonews"})
+                chunks = soup1.findAll('td')
+                stockName = chunks[1].find('b').string  # 股票名称
+                if stockName == None:
+                    stockName = str(stockName)
+                print(stockName + '\n')
+                reportAuthor = chunks[3].find('b').string  # 研究报告作者
+                if reportAuthor == None:
+                    reportAuthor = str(reportAuthor)
+                print(reportAuthor + '\n')
+                stockCode = chunks[6].find('b').string  # 股票代码
+                if stockCode == None:
+                    stockCode = str(stockCode)
+                print(stockCode + '\n')
+                reportBelongCompany = chunks[8].find('b').string  # 研究报告出处
+                if reportBelongCompany == None:
+                    reportBelongCompany = str(reportBelongCompany)
+                print(reportBelongCompany + '\n')
+                rateLevel = chunks[11].find('b').string  # 推荐评级
+                if rateLevel == None:
+                    rateLevel = str(rateLevel)
+                print(rateLevel + '\n')
+                reportType = chunks[13].find('b').string  # 研究报告栏目
+                if reportType == None:
+                    reportType = str(reportType)
+                print(reportType + '\n')
+                Title = soup.find('strong').string
+                print(Title + '\n')
+                time = chunks[18].find('b').string
+                tmptimes = time.split(' ')
+                time = tmptimes[0]
+                timechunks = time.split('-')
+                if len(timechunks) == 3:
+                    if len(timechunks[1]) == 1:
+                        timechunks[1] = '0' + timechunks[1]
+                    if len(timechunks[2]) == 1:
+                        timechunks[2] = '0' + timechunks[2]
+                time = timechunks[0] + timechunks[1] + timechunks[2]
+                Title = Title
+                ContentAbstractArea = soup.find('div', {"class": "new_content"})
+                if ContentAbstractArea != None:
+                    tmp = ContentAbstractArea.find('td')
+                    tmpStr = str(tmp)
+                    abstract = tmpStr[34:-12].replace(
+                        '<font style="DISPLAY:none;"><a href="http://www.microbell.com/">www.microbell.com</a>(\xe8\xbf\x88\xe5\x8d\x9a\xe6\xb1\x87\xe9\x87\x91)\xe3\x80\x82</font>',
+                        '\n')
+                    abstract = abstract.replace('&nbsp;', '')
+                    abstract = abstract.replace('<br />', '')
+                    abstract = abstract.replace('\n', '')
+                    TotalAbstract = TotalAbstract + abstract + '\n'
+                ##INSERT THE INFO TYPE:GBK
+                print('#################################')
+                try:
+                    self.cursor.execute(
+                        "insert into MBStrategy values('%s','%s','%s','%s','%s','%s','%s','%s','%s')" % (
+                        page, Title, TotalAbstract.decode('utf-8'), time, reportAuthor, stockCode, reportBelongCompany,
+                        rateLevel, reportType))
+                    self.conn.commit()
+                except Exception as e:
+                    print(e)
+
+                print('************************************')
+            except:
+                writeindex = file('pagesErro.txt', 'a+')
+                writeindex.write(page + '\n')
+                writeindex.close()
+                continue
+
+def main():
+    # oriUrl='http://www.microbell.com/result.asp?lm=0&area=DocTitle&timess=13&key=&page=1'
+    oriUrl = 'http://www.hibor.com.cn/result.asp?lm=0&area=DocTitle&timess=13&key=&dtype=&page=1'
+
+    for i in range(1, 80):
+        hosts.put(oriUrl[:-1] + str(i))
+
+
+    # spawn a pool of threads.
+    for i in range(1):
+        t = ThreadUrl(hosts, pages)
+        t.setDaemon(True)
+        t.start()
+
+    for i in range(1):
+        dt = ThreadUrlSecond(hosts, pages)
+        dt.setDaemon(True)
+        dt.start()
+        print('bbbb')
+
+    # wait on the queue until everything has been processed
+    hosts.join()
+    pages.join()
+
+
+main()
+filr2.close()
+filr1.close()
+
 
--- tools/to_mysql.py
+++ tools/to_mysql.py
@@ -73,7 +73,7 @@ class barToMysql(object):
 
 
 class factorToMysql(object):
-    """储存因子数据到本地数据库中"""
+    ##储存因子数据到本地数据库中
     def __init__(self):
         self.db_host = "localhost"
         self.db_user = "root"
@@ -120,6 +120,57 @@ class factorToMysql(object):
         self.cursor.execute(sql)
         self.db.commit()
 
+
+    def get_data(self):
+        pass
+
+    def close(self):
+        self.db.close()
+
+    def update(self):
+        pass
+
+
+class UrlTomysql(object):
+    ##储存网址数据到本地数据库中
+
+    def __init__(self):
+        self.db_host = "localhost"
+        self.db_user = "root"
+        self.db_pass = "8261426"
+        self.db_name = "stock"
+        # 打开数据库连接
+        self.db = pymysql.connect(self.db_host, self.db_user, self.db_pass, self.db_name
+                                  , use_unicode=True, charset="utf8")
+        # 使用 cursor() 方法创建一个游标对象 cursor
+        self.cursor = self.db.cursor()
+
+    def creat(self, table_name):
+        self.cursor.execute("DROP TABLE IF EXISTS %s" % table_name)
+        sql = """CREATE TABLE %s (
+        id  integer(8),
+        url  varchar(255),
+        PRIMARY KEY (id)  
+                       )""" % table_name
+        self.cursor.execute(sql)
+        self.cursor.execute('SET NAMES utf8;')
+        self.cursor.execute('SET CHARACTER SET utf8;')
+        self.cursor.execute('SET character_set_connection=utf8;')
+        self.db.commit()
+
+    def insert(self, table_name, data):
+        for i in range(len(data)):
+            data_url = data["url"][i]
+            sql = """INSERT INTO %s (url)
+            VALUES (%s)""" % (table_name,repr(data_url))
+            sql = sql.replace('None', 'NULL')
+            self.cursor.execute(sql)
+            self.db.commit()
+
+    def delete(self, table_name, date):
+        sql = """delete from %s where date=%s""" % (table_name, repr(date))
+        self.cursor.execute(sql)
+        self.db.commit()
 
     def get_data(self):
         pass
